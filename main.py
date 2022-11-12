9ffc4075c8d0d1292b7a9e314f454196c211ba80
Zect3279 <zeck.3279@gmail.com>
2022-11-12 22:06:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -42,29 +42,19 @@ def post_tweet(title: str, video_id: str):
     api.update_status(tweet2)
 
 def post_discord(data):
+  title = data["title"]
+  video_id = data["video_id"]
+  view = data["baseinfo"]["view_counter"]
+  comment = data["baseinfo"]["comment_num"]
+  mylist = data["baseinfo"]["mylist_counter"]
+  timestamp = data["start_time"]
+  video_img = data["baseinfo"]["thumbnail_url"]
   try:
-    title = data["title"]
-    video_id = data["video_id"]
-    view = data["baseinfo"]["view_counter"]
-    comment = data["baseinfo"]["comment_num"]
-    mylist = data["baseinfo"]["mylist_counter"]
     author_name = data["baseinfo"]["user_nickname"]
     author_icon = data["baseinfo"]["user_icon_url"]
-    timestamp = data["start_time"]
-    video_img = data["baseinfo"]["thumbnail_url"]
   except:
-    row_data = requests.get(NOW_PLAYING_API_URL_TWO)
-    data = row_data.json()
-    print('2')
-    title = data["title"]
-    video_id = data["video_id"]
-    view = data["baseinfo"]["view_counter"]
-    comment = data["baseinfo"]["comment_num"]
-    mylist = data["baseinfo"]["mylist_counter"]
-    author_name = data["baseinfo"]["user_nickname"]
-    author_icon = data["baseinfo"]["user_icon_url"]
-    timestamp = data["start_time"]
-    video_img = data["baseinfo"]["thumbnail_url"]
+    author_name = "N/A"
+    author_icon = ""
   print(title, video_id, view, comment, mylist, author_name, author_icon, timestamp, video_img,'\n')
   discord = {"username": "Kiiteitte","avatar_url": "https://pbs.twimg.com/profile_images/1584526973505634304/M686vgg3_400x400.jpg","content": None}
   discord.update({"embeds":[{"title": f"♪ {title}","url": f"https://nico.ms/{video_id}","fields": [{"name": "▶ 再生数","value": f"{view}","inline": True},{"name": "📔 コメント数","value": f"{comment}","inline": True},{"name": "🖊️ マイリス数","value": f"{mylist}","inline": True}],"author": {"name": f"{author_name}","icon_url": f"{author_icon}"},"timestamp": f"{timestamp}","thumbnail": {"url": f"{video_img}"}}]})
